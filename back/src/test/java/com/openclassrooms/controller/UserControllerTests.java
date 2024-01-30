1c4a601097be5859cff0464f97c11535092a801f
Sirinemn <sirinemn@outlook.fr>
2024-01-30 22:34:09 +0100

    userControllerTest

--- back/src/test/java/com/openclassrooms/controller/UserControllerTests.java
+++ back/src/test/java/com/openclassrooms/controller/UserControllerTests.java
@@ -5,16 +5,14 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.LocalDateTime;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-=======
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
->>>>>>> fix/session-cypress-testing
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
@@ -23,28 +21,29 @@ import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.openclassrooms.starterjwt.SpringBootSecurityJwtApplication;
 import com.openclassrooms.starterjwt.dto.UserDto;
+import com.openclassrooms.starterjwt.mapper.UserMapper;
+import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.services.UserService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @EnableAutoConfiguration(exclude=SecurityAutoConfiguration.class)
 @ContextConfiguration(classes= SpringBootSecurityJwtApplication.class)
-<<<<<<< HEAD
-@TestPropertySource("classpath:application-test.properties")
-=======
->>>>>>> fix/session-cypress-testing
+
 public class UserControllerTests {
 
 	@Autowired
 	private MockMvc mockMvc;
-
+	@Autowired
+	private ObjectMapper objectMapper;
 	@MockBean
 	private UserService service;
+	@MockBean
+	private UserMapper userMapper;
 
 
 	@Test
 	public void getUserControllerTest() throws Exception {
-<<<<<<< HEAD
 		LocalDateTime rightNow = LocalDateTime.now();
 		User user = User.builder().id(10L).email("test@mail.fr").firstName("test").lastName("test").password("test123")
 				.admin(true).createdAt(rightNow).updatedAt(rightNow).build();
@@ -57,16 +56,4 @@ public class UserControllerTests {
 	}
 
 }
-=======
-	    ObjectMapper objectMapper = new ObjectMapper();
-	    		UserDto userDto = UserDto.builder().id(2L).email("test@mail.fr").firstName("test").lastName("test")
-				.password("test123").admin(true).createdAt(null).updatedAt(null).build();
-	    		
-		String jsonResponse = objectMapper.writeValueAsString(userDto);
-		mockMvc.perform(get("/api/user/2").contentType(MediaType.APPLICATION_JSON_VALUE))
-		.andExpect(status().isOk())
-				.andExpect(content().json(jsonResponse, true));
-	}
 
-}
->>>>>>> fix/session-cypress-testing
